5723e55a6aba8aaf1d8a7c10f00e0438505e5c1f
chenguo50151 <chenguo50151@hundsun.com>
2024-07-25 17:03:24 +0800

    dev: 通过组件库url地址异步加载和渲染组件方案验证通过

--- packages/plex-ui-vue2-docs/src/App.vue
+++ packages/plex-ui-vue2-docs/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
-    <h1>PlexUI 组件库预览</h1>
-    <div>
-      <plex-button type="success">Plex 按钮</plex-button>
-      <plex-input  v-model="inputValue" placeholder="请输入内容"/>
+    <div v-if="isLoaded">
+      <PlexButton type="success" @click="handleClick">点我试试</PlexButton>
+      <PlexInput placeholder="请输入内容" />
+    </div>
+    <div v-else>
+      正在加载 PlexUIVue2 组件库：{{ PlexUIVue2LibUrl }}
     </div>
   </div>
 </template>
@@ -13,11 +15,46 @@ export default {
   name: 'App',
   data() {
     return {
-      inputValue: ''
+      isLoaded: false,
+      PlexUIVue2LibUrl: 'http://localhost:3000/js/plex-ui-vue2.8f4a43b7d39fb17285db.umd.js'
     };
-  }
+  },
+  methods: {
+    loadScript(url) {
+      return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.head.appendChild(script);
+      });
+    },
+    async loadPlexUI() {
+      try {
+        await this.loadScript(this.PlexUIVue2LibUrl);
+        if (window.PlexUIVue2) {
+          console.log('>>> PlexUIVue2: ', window.PlexUIVue2);
+          this.$options.components.PlexButton = window.PlexUIVue2.PlexButton;
+          this.$options.components.PlexInput = window.PlexUIVue2.PlexInput;
+          this.isLoaded = true;
+        } else {
+          console.error('PlexUIVue2 字段不在 window 对象中');
+        }
+      } catch (err) {
+        console.error('PlexUIVue2 组件库脚本加载失败:', err);
+      }
+    },
+    handleClick() {
+      alert('你还真点啊!');
+    }
+  },
+  mounted() {
+    this.loadPlexUI();
+  },
 };
 </script>
+
 
 <style lang="less">
 #app {
