33998a3b5b5129ed2ee8f0612dc4581ed6f3eb3f
chenguo50151 <chenguo50151@hundsun.com>
2024-08-01 19:29:00 +0800

    dev: ElementUI schema数据结构调整

--- packages/plex-elementui-adaptor/src/schemas/el-button/style.js
+++ packages/plex-elementui-adaptor/src/schemas/el-button/style.js
@@ -22,13 +22,13 @@ const style = [
     label: '边框宽度',
     key: 'borderWidth',
     type: 'number',
-    value: 0
+    value: NaN
   },
   {
     label: '边框圆角',
     key: 'borderRadius',
     type: 'number',
-    value: 0
+    value: NaN
   },
   {
     label: '边框类型',
@@ -61,49 +61,49 @@ const style = [
     label: '内边距-上',
     key: 'paddingTop',
     type: 'number',
-    value: 0
+    value: NaN
   },
   {
     label: '内边距-右',
     key: 'paddingRight',
     type: 'number',
-    value: 0
+    value: NaN
   },
   {
     label: '内边距-下',
     key: 'paddingBottom',
     type: 'number',
-    value: 0  
+    value: NaN
   },
   {
     label: '内边距-左',
     key: 'paddingLeft',
     type: 'number',
-    value: 0
+    value: NaN
   },
   {
     label: '外边距-上',
     key: 'marginTop',
     type: 'number',
-    value: 0
+    value: NaN
   },
   {
     label: '外边距-右',
     key: 'marginRight',
     type: 'number',
-    value: 0
+    value: NaN
   },
   {
     label: '外边距-下',
     key: 'marginBottom',
     type: 'number',
-    value: 0
+    value: NaN
   },
   {
     label: '外边距-左',
     key: 'marginLeft',
     type: 'number',
-    value: 0
+    value: NaN
   }
 ];
 
